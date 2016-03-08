d92e6023e5f02f2e8d95fc10d809536dde03b93f
Sekhar Paladugu <sekharp@gmail.com>
2016-03-08 10:56:29 -0700

    links submitted and created and rendering

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,4 @@
 class Link < ActiveRecord::Base
   default_scope { order("created_at") }
-  # validates :url, :format => URI::regexp(%w(http https))
+  validates :url, :format => URI::regexp(%w(http https))
 end
